772966471c67f9e9a5f9c204f3985f4727d34529
tanminz <duongtanminh2306@gmail.com>
2025-06-18 10:39:52 +0700

    Add files via upload

---
+++ run.py
@@ -0,0 +1,8 @@
+from App import create_app   # với điều kiện thư mục bạn đặt là "App" chứ không phải "app"
+
+
+app = create_app()
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5000)
+
